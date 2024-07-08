020873d1614f04532a64dd56cd5157b66dab40a6
Ilija Lazoroski <ilija.la@live.com>
2024-07-08 09:54:32 +0000

    Initialize the repository

---
+++ README.md
@@ -0,0 +1,1 @@
+# mimikatz credentials collector
---
+++ vulture_allowlist.py
@@ -0,0 +1,8 @@
+from mimikatz_credentials_collecotr.plugin import Plugin
+
+Plugin
+Plugin.plugin_name
+Plugin.local_machine_info
+Plugin.kwargs
+Plugin.run
+Plugin.interrupt
